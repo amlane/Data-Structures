fb194291f108550a299423179b12bca765d5d8ec
amlane <twelvexstring@gmail.com>
2020-01-27 16:28:10 -0600

    completed queue portion

--- doubly_linked_list/dll_queue.py
+++ doubly_linked_list/dll_queue.py
@@ -10,17 +10,21 @@ class Queue:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
-    # enqueue should add an item to the back of the queue.
+        self.storage = DoublyLinkedList()
 
+    # enqueue should add an item to the back of the queue.
     def enqueue(self, value):
         self.size += 1
-        DoublyLinkedList.add_to_tail(self, value)
+        self.storage.add_to_tail(value)
+
     # dequeue should remove and return an item from the front of the queue.
-
     def dequeue(self):
-        self.size -= 1
-        return DoublyLinkedList.remove_from_head(self)
-    # len returns the number of items in the queue
+        if self.size == 0:
+            return
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
+    # len returns the number of items in the queue
     def len(self):
         return self.size
